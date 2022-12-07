2a1a907eff7fafe40ade04ed9601bbf337602237
Pauline Raulet <raulet.pauline@gmail.com>
2022-12-07 17:29:03 +0100

    JS index

--- front/JS/script.js
+++ front/JS/script.js
@@ -13,21 +13,26 @@ fetch("http://localhost:3000/api/products")
 
         let elementLien = document.createElement("a");
         elementLien.href = "./product.html?id=42";
+
         let elementArticle = document.createElement("article");
         elementLien.appendChild(elementArticle);
+
         let elementImage = document.createElement("img");
         elementImage.src = (image);
         elementImage.alt = (imageAlt);
         elementArticle.appendChild(elementImage);
+
         let elementTitre = document.createElement("h3");
         elementTitre.innerHTML = (nom);
         elementTitre.classList.add("productName");
         elementArticle.appendChild(elementTitre);
+
         let elementDescription = document.createElement("p");
         elementDescription.innerHTML = (description);
         elementDescription.classList.add("productDescription");
         elementArticle.appendChild(elementDescription);
-        let elementCible = document.getElementById("items").appendChild(elementLien);
+        
+       document.getElementById("items").appendChild(elementLien);
 
     }
 });
